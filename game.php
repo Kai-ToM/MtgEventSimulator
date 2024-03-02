110e7e100acb41e9aaa110a066e587386e4d14f4
kaitodev <pettai20451@gmail.com>
2024-03-03 00:00:27 +0900

    [update]playergetterに処理を分離、ランダムな勝率期待値のプレイヤーを取得する機能の実相

--- game.php
+++ game.php
@@ -1,15 +1,18 @@
 <?php
-include 'player.php';
+include 'playerGetter.php';
 
 class Game
 {
     const MAX_WINS = 7;
     const MAX_LOSSES = 3;
+
     protected $players_winrate = [];
 
-    protected function getPlayer()
+    protected $player_getter;
+    
+    public function __construct()
     {
-        return new Player();
+        $this->player_getter = new PlayerGetter();
     }
 
     protected function saveWinRate(Player $player)
@@ -19,19 +22,19 @@ class Game
 
     public function playGames($times)
     {
-        $player1 = $this->getPlayer();
-        $player2 = $this->getPlayer();
+        $player1 = $this->player_getter->get();
+        $player2 = $this->player_getter->get();
 
         for ($i = 0; $i < $times; $i++) {
             $this->duel($player1, $player2);
 
             if ($this->isGameFinished($player1)) {
                 $this->saveWinRate($player1);
-                $player1 = $this->getPlayer();
+                $player1 = $this->player_getter->get();
             }
             if ($this->isGameFinished($player2)) {
                 $this->saveWinRate($player2);
-                $player2 = $this->getPlayer();
+                $player2 = $this->player_getter->get();
             }
         }
     }
