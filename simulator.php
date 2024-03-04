028ec5c6057ede328a7ee4bd84e624b8ebec5ad5
kaitodev <pettai20451@gmail.com>
2024-03-05 00:37:06 +0900

    [update]出力部分を実装

--- simulator.php
+++ simulator.php
@@ -2,6 +2,35 @@
 include 'game.php';
 
 $game = new Game();
-$game->playGames(10000);
-$players = $game->getSavedPlayers();
-file_put_contents('./result.csv', implode(PHP_EOL, $winrate));
+$players = $game->playGames(10000);
+
+$file = fopen('./player_result.csv', 'w');
+foreach ($players as $player) {
+    $data = [
+        $player->getExpectedWinRate(), 
+        $player->getWins(),
+        $player->getLosses(),
+        $player->getWins() . '-' . $player->getLosses(),
+        $player->getWins() / ($player->getWins() + $player->getLosses())
+    ];
+    fputcsv($file, $data);
+}
+
+$file = fopen('./duel_result.csv', 'w');
+
+$cnt = 0;
+$hoge = 0;
+foreach ($players as $player) {
+    foreach ($player->getResults() as $result) {
+        $cnt++;
+        $data = [
+            $fuga = $player->getExpectedWinRate(),
+            $result['result'],
+        ];
+        fputcsv($file, $data);
+        $hoge += $fuga;
+    }
+}
+echo $hoge / $cnt . PHP_EOL;
+
+fclose($file);
